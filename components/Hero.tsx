4f84a01a89a597a63a022e448fb28c446d987e20
Narayan Aryal <aryal.coding@gmail.com>
2023-09-12 12:40:37 +1000

    project initilized

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = () => {
     const handleScroll =() => {}
   return (
     <div className='hero'>
-        <div className='flex-1 pt-36 padding-x'></div>
+        <div className='flex-1 pt-36 padding-x'>
         <h1 className='hero__title'> Find, Book or Rent a car -quickly and easily</h1>
 <p className='hero__subtitle'> Streamline your car rental experience with our effortless booking process.</p>
 <CustomButton
@@ -18,6 +18,7 @@ handleClick = {handleScroll}
 
 
 />
+</div>
     </div>
   )
 }
